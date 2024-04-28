df2bb15e2c22f6dacee9f80aac86c2f7dfead51a
Emery Chikezie Ogechukwu <emerychikezie2@gmail.com>
2024-04-28 23:35:14 +0100

    Major changes to code base for RepoDetails
    Major changes to code base for RepoDetails. Also removed Single repo page and adjusted the routing for the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import RepoDetails from "./pages/RepoDetails";
-import SingleRepoPage from "./pages/SingleRepoPage";
+import ErrorBoundary from "./pages/ErrorBoundary";
+import NotFound from "./pages/NotFound";
 
-// const singleUser = `https://api.github.com/users/emery-hub`
-// const repos = `https://api.github.com/users/emery-hub/repos?per_page=5`
-// https://api.github.com/users/emery-hub/repos?page=1&per_page=10&sort=updated
-// `https://api.github.com/users/${users}/repos/${repo}`
-// `https://api.github.com/users/emery-hub/repos/search?q=${repo}&${name}`
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router>
+        <div>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/:name" element={<RepoDetails />}></Route>
-          <Route path="/" element={<SingleRepoPage />}></Route>
+          <Route path="/name" element={<Home />}></Route>
+          <Route path="/name/:repoName" element={<ErrorBoundary>
+            <RepoDetails />
+          </ErrorBoundary>}></Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+        </div>
+      </Router>
     </>
   );
 }
--- src/pages/RepoDetails.jsx
+++ src/pages/RepoDetails.jsx
@@ -2,56 +2,30 @@ import  { useState, useEffect } from "react";
 import {  useParams } from "react-router-dom";
 
 
-
 export default function RepoDetails() {
-  const [repo, setRepo] = useState([]);
-  const [users] = useState("emery-hub");
-  const { name } = useParams();
+  const [repository, setRepository] = useState([]);
+  const { repoName } = useParams();
 
- 
-  //   const params = useParams
 
   useEffect(() => {
-    const fetchRepoDetailData = async () => {
-      const res = await fetch(
-        `https://api.github.com/users/${users}/repos?page=1&per_page=10&sort=updated`
+    fetch(`https://api.github.com/repos/emery-hub/${repoName}`)
+      .then((response) => response.json())
+      .then((data) => setRepository(data))
+      .catch((error) =>
+        console.error("Error fetching repository details:", error)
       );
-      const data = await res.json();
+  }, [repoName]);
 
-      setRepo(data);
-      console.log(data);
-    };
+   if (!repository) {
+    return <div>Loading...</div>;
+  }
 
-    fetchRepoDetailData();
-  }, [name, users, repo]);
-
-  
-  
-    return (
-    <>
-      <section className="max-w-5xl mx-auto">
-        <h1>{name}</h1>
-        
-
-        <div>
-          
-        </div>
-      </section>
-
-      
-    </>
+  return (
+    <div className="p-5 bg-white rounded-lg shadow shadow-emerald-300">
+      <h2>Name: {repository.name}</h2>
+      <p>Description: {repository.description}</p>
+      <p>Language: {repository.language}</p>
+      <p>Stars: {repository.stargazers_count}</p>
+    </div>
   );
-
 }
-
-
-
-
-
-// {repo.map((repo) => (
-//             <article className="p-5 bg-white rounded-lg shadow shadow-emerald-300">
-//               <article className="flex items-center justify-start">
-//                 <h2>{repo.name}</h2>
-//               </article>
-//             </article>
-//           ))}
